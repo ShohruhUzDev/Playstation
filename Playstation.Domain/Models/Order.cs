ad542b51ebeb81bf335faecf0aa9fb88eda1676a
ShohruhUzDev <shohruh7464784@mail.ru>
2021-11-02 13:07:57 +0500

    add models

---
+++ Playstation.Domain/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playstation.Domain.Models
+{
+    public  class Order
+    {
+       public int Id { get; set; }
+       public DateTime StartTime { get; set; }
+       public DateTime EndTime { get; set; }
+       public double Amount { get; set; }
+       public bool Closed { get; set; }
+       public int DeviceId { get; set; }
+       public Device Device { get; set; }
+
+
+
+
+    }
+
+
+}
---
+++ Playstation.Domain/Models/Tarrif.cs
@@ -0,0 +1,12 @@
+﻿namespace Playstation.Domain.Models
+{
+    public class Tarrif
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public string Title { get; set; }
+        public int TotalMinutes { get; set; }
+    }
+
+
+}
